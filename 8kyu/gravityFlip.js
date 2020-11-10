affbc7ec6ef5a6e85d3d3bd936a77100a84cc155
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-10 16:55:36 +0300

    8kyu Gravity Flip

--- 8kyu/gravityFlip.js
+++ 8kyu/gravityFlip.js
@@ -10,6 +10,7 @@
 // +---++---++---++---+         +---++---++---++---+
 // |   ||   ||   ||   |         |   ||   ||   ||   |
 // +---++---++---++---+         +---++---++---++---+
+//
 // Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
 //
 // Examples:
