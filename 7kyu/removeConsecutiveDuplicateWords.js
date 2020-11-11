3aa0ab03c0bf93a859ccdce0538f5ed6c727d212
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-11 15:29:19 +0300

    7kyu Remove consecutive duplicate words

---
+++ 7kyu/removeConsecutiveDuplicateWords.js
@@ -0,0 +1,8 @@
+// Your task is to remove all consecutive duplicate words from string, leaving only first words entries. For example:
+//
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+function removeConsecutiveDuplicates(s) {
+  return s.split(' ').filter((value, index, arr) => value != arr[index + 1]).join(' ');
+}
