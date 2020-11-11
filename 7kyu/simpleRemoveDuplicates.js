cbf5fd8c7401f8025937ab157ac89f9241c48232
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-11 22:08:29 +0300

    7kyu Simple remove duplicates

---
+++ 7kyu/simpleRemoveDuplicates.js
@@ -0,0 +1,7 @@
+// In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+// Remove the 3's at indices 0 and 3 followed by removing a 4 at index 1
+// solve([3, 4, 4, 3, 6, 3]); // => [4, 6, 3]
+
+function solve(arr){
+  return Array.from(new Set(arr.reverse())).reverse();
+}
