8be8ea98f38288c8b34c87d727504f68411723d1
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-12 15:51:00 +0300

    6kyu What century is it?

---
+++ 6kyu/whatCenturyIsIt.js
@@ -0,0 +1,14 @@
+// Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+//
+// Examples
+// "1999" --> "20th"
+// "2011" --> "21st"
+// "2154" --> "22nd"
+// "2259" --> "23rd"
+// "1124" --> "12th"
+// "2000" --> "20th"
+
+function whatCentury(year) {
+  var century = Math.ceil(year / 100);
+  return century + (century > 3 && century < 20 ? 'th' : ['th', 'st', 'nd', 'rd'][century % 10]);
+}
