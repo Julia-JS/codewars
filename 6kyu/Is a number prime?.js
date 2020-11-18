ec25d113b011b05613ef2277d7657ac2b39bc7af
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-18 12:14:58 +0300

    6kyu Is a number prime?

--- 6kyu/Is a number prime?.js
+++ 6kyu/Is a number prime?.js
@@ -22,3 +22,12 @@ function isPrime(num) {
   }
   return true;
 }
+
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (!(num % i)) {
+      return false;
+    }
+  }
+  return num > 1;
+}
