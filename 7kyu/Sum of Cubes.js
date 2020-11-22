dda5466ae0e177d3a3aea8f1d6263aa857e96304
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-22 19:36:59 +0300

    7kyu Sum of Cubes

---
+++ 7kyu/Sum of Cubes.js
@@ -0,0 +1,12 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+//
+// Assume that the input n will always be a positive integer.
+//
+//   Examples:
+//
+// sumCubes(2) // 9
+// // sum of the cubes of 1 and 2 is 1 + 8
+
+function sumCubes(n){
+  return Array.from({length: n}, (a, i) => (i + 1) ** 3).reduce((a, b) => a + b);
+}
