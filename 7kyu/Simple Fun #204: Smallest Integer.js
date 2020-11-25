7945e67cd766b8256746ec06575accc4957cbec4
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-25 21:53:16 +0300

    7kyu Simple Fun #204: Smallest Integer

---
+++ 7kyu/Simple Fun #204: Smallest Integer.js
@@ -0,0 +1,32 @@
+// Task
+// You're given a two-dimensional array of integers matrix. Your task is to determine the smallest non-negative integer that is not present in this array.
+//
+// Input/Output
+//   [input] 2D integer array matrix
+//
+// A non-empty rectangular matrix.
+//
+// 1 ≤ matrix.length ≤ 10
+//
+// 1 ≤ matrix[0].length ≤ 10
+//
+//   [output] an integer
+//
+// The smallest non-negative integer that is not present in matrix.
+//
+//   Example
+// For
+//
+// matrix = [ [0, 2], [5, 1]] the result should be 3
+//
+// 0,1,2,(3)...5
+
+function smallestInteger(matrix) {
+  let arr = [...new Set(matrix.join(',').split(','))].sort((a, b) => a - b).filter(a => a >= 0);
+  for (let i = 0; i < Math.max(...arr); i++) {
+    if (i != arr[i]) {
+      return i;
+    }
+  }
+  return arr.length ? Math.max(...arr) + 1 : 0;
+}
