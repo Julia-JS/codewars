37c29a8bb823f24aac6fff1689fe2f2b3de1ad1e
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-11-26 10:49:31 +0300

    7kyu Array comparator

---
+++ 7kyu/Array comparator.js
@@ -0,0 +1,6 @@
+// You have two arrays in this kata, every array contain only unique elements.
+// Your task is to calculate number of elements in first array which also are in second array.
+
+function matchArrays(v,r){
+  return v.filter(a => r.includes(a)).length;
+}
