39019e9e89c5a9a5d2df105d4937b5914f331250
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-12-02 01:34:55 +0300

    6kyu Pascals Triangle

---
+++ 6kyu/Pascal's Triangle.js
@@ -0,0 +1,29 @@
+// In mathematics, Pascal's triangle is a triangular array of the binomial coefficients expressed with formula (n k) = n!/(n-k)!, where n denotes a row of the triangle, and k is a position of a term in the row.
+//
+// Pascal's Triangle
+//
+// You can read Wikipedia article on Pascal's Triangle for more information.
+//
+// Task
+// Write a function that, given a depth n, returns n top rows of Pascal's Triangle flattened into a one-dimensional list/array.
+//
+// n guarantees that terms of the Triangle won't overflow.
+//
+// Example:
+// n = 1: [1]
+// n = 2: [1,  1, 1]
+// n = 4: [1,  1, 1,  1, 2, 1,  1, 3, 3, 1]
+
+function pascalsTriangle(n) {
+  let factorial = (k) => {
+    let f = 1;
+    for (let i = 1; i <= k; i++) {
+      f = f * i;
+    }
+    return f;
+
+  };
+  let arr = Array.from({length: n}, (a, i) => Array.from({length: i + 1}, (b, j) => j === 0 || j === i ? 1 : Math.round(factorial(i) / (factorial(i - j) * factorial(j)))));
+
+  return [].concat(...arr);
+}
