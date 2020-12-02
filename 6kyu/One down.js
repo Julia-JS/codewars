440a17bddf6b54c29a6d472883b0201fc5871f33
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-12-02 13:47:20 +0300

    6kyu One down

---
+++ 6kyu/One down.js
@@ -0,0 +1,28 @@
+// A very passive-aggressive co-worker of yours was just fired. While he was gathering his things, he quickly inserted a bug into your system which renamed everything to what looks like jibberish.
+// He left two notes on his desk, one reads: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" while the other reads: "Uif usjdl up uijt lbub jt tjnqmf kvtu sfqmbdf fwfsz mfuufs xjui uif mfuufs uibu dpnft cfgpsf ju".
+//
+// Rather than spending hours trying to find the bug itself, you decide to try and decode it.
+//
+// If the input is not a string, your function must return "Input is not a string".
+// Your function must be able to handle capital and lower case letters. You will not need to worry about punctuation.
+
+function oneDown(str) {
+  if (typeof str !== 'string') {
+    return 'Input is not a string';
+  }
+  let res = [];
+  let arr = str.split('');
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < arr.length; i++) {
+    let arrEl = arr[i].split('');
+    if (letters.indexOf(arr[i]) === 0) {
+      res.push(letters[letters.length - 1]);
+    } else if (letters.indexOf(arr[i]) === -1) {
+      res.push(arr[i]);
+    } else {
+      res.push(letters[letters.indexOf(arr[i]) - 1]);
+    }
+  }
+  return res.join('');
+}
