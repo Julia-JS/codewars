2cf9221ae96d2414b41a501d5bd1d79093935d86
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-12-06 16:10:47 +0300

    6kyu How many feelings?

---
+++ 6kyu/How many feelings?.js
@@ -0,0 +1,33 @@
+// You have two arguments: string - a string of random letters(only lowercase) and array - an array of strings(feelings). Your task is to return how many specific feelings are in the array.
+//
+//   For example:
+//
+//   string -> 'yliausoenvjw'
+// array -> ['anger', 'awe', 'joy', 'love', 'grief']
+// output -> '3 feelings.' // 'awe', 'joy', 'love'
+//
+//
+// string -> 'griefgriefgrief'
+// array -> ['anger', 'awe', 'joy', 'love', 'grief']
+// output -> '1 feeling.' // 'grief'
+//
+//
+// string -> 'abcdkasdfvkadf'
+// array -> ['desire', 'joy', 'shame', 'longing', 'fear']
+// output -> '0 feelings.'
+// If the feeling can be formed once - plus one to the answer.
+//
+//   If the feeling can be formed several times from different letters - plus one to the answer.
+//
+//   Eeach letter in string participates in the formation of all feelings. 'angerw' -> 2 feelings: 'anger' and 'awe'.
+
+function countFeelings(string, array) {
+  let res = array.map(a => a.split('').filter(a => string.includes(a)).join(''));
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (res[i] === array[i]) {
+      sum++
+    }
+  }
+  return `${sum} feeling${sum === 1 ? '' : 's'}.`;
+}
