8441d394e75160b1a25d60b9a4ed5d573f802023
Юлия <ulia@MacBook-Pro-Ulia.local>
2020-12-20 11:20:00 +0300

    7kyu Unique numbers

---
+++ 7kyu/Unique numbers.js
@@ -0,0 +1,10 @@
+// You have to write a function uniqueNumbers which returns unique numbers array. You don't need to validate input arg arr. Retain the original order of the input.
+//
+// Examples:
+//
+//   uniqueNumbers([1,1,2,2]) => [1,2]
+// uniqueNumbers([1,2,3,1]) => [1,2,3]
+
+function uniqueNumbers(numbersArray) {
+  return [...new Set(numbersArray)];
+}
