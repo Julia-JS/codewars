2580cdb3946e99c213b0f0aa3eed930e04f34ef7
Юлия <ulia@MacBook-Pro-Ulia.local>
2021-01-20 12:00:52 +0300

    6kyu Shopping List

---
+++ 6kyu/Shopping List.js
@@ -0,0 +1,52 @@
+// Calculate the cost of a shopping list by writing a function. The function takes an argument which is an array consisting of sub-arrays. For example:
+//
+//   shoppingListCost([["Orange Juice", 2],["Chocolate", 4],["Pears", 8]])
+// The input array includes sub arrays containing the name and quantity of each item being purchased. Already existing in the global scope is an object which gives some information regarding the shopping items:
+//
+//   var groceries = {
+//     "Orange Juice": {
+//       "price" : 1.5,
+//       "discount": 10,
+//       "bogof": false
+//     },
+//
+//     "Chocolate": {
+//       "price" : 2,
+//       "discount" : 0,
+//       "bogof": true
+//     },
+//
+//     // more items...
+//   }
+// This object contains the price of the item, the discount currently being offered on that item, and whether the item is currently "buy one get one free" (meaning for every item purchased, one of the same item is free).
+//
+// Return the cost of the shopping list. If the input array contains no items, return zero. Round the answer to 2 decimal places.
+//
+//   All input arrays will be valid and contain items included in the global groceries object. Each item will only appear once in any input array.
+//
+//   Examples
+//
+// shoppingListCost([["Orange Juice", 2],["Chocolate", 4]])
+// returns 6.7
+//
+// shoppingListCost([["Chocolate", 5],["Orange Juice", 15]])
+// returns 26.25
+
+function shoppingListCost(arr) {
+  let price = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let item = 0;
+    item = groceries[arr[i][0]]['price'] * (100 - groceries[arr[i][0]]['discount']) / 100;
+
+    if (groceries[arr[i][0]]['bogof']) {
+      item *= Math.ceil(arr[i][1] / 2);
+    } else {
+      item *= arr[i][1];
+    }
+
+    price += item;
+    item = 0;
+  }
+
+  return +price.toFixed(2);
+}
